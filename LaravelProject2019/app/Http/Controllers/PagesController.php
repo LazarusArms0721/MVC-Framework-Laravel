5ed6118390a5d7012e4da324b7655c801b966ace
Erhan <erhan.akin@live.nl>
2019-09-16 19:52:27 +0200

    Applied modulair design for views

---
+++ LaravelProject2019/app/Http/Controllers/PagesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    //Get index page
+    //Put the url in there
+    public function getIndex(){
+        return view ('pages.index');
+    }
+}
---
+++ LaravelProject2019/resources/views/pages/index.blade.php
@@ -0,0 +1,12 @@
+@extends ('layouts.app')
+
+@section ('title', 'Homepage')
+
+@section ('content')
+
+    <div class="title m-b-md">
+        Hello World!
+    </div>
+
+@endsection
+
