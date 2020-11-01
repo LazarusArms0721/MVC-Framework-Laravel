1113436c7668d4589e0cf4d392556e4857fefbf2
Erhan <erhan.akin@live.nl>
2020-11-01 21:35:51 +0100

    WIP

--- routes/web.php
+++ routes/web.php
@@ -67,7 +67,7 @@ Route::get('/', 'PagesController@getIndex', function(){
 
 Route::get('/dashboard',
             'DashboardController@showDashboard')
-                ->middleware('auth')->middleware('check_user_role:' . \app\role\userrole::ROLE_EDITOR);
+                ->middleware('auth')->middleware('check_user_role:' . \App\Role\UserRole::ROLE_EDITOR);
 
 Route::get('/dashboard/{user}/edit',
             'DashboardController@getUser')
